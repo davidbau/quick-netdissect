011b898197e6c28d477944effd7875b5cdb8d2c0
David Bau <davidbau@mit.edu>
2018-08-05 15:10:39 -0400

    Support auto-import for package namespaces.

--- netdissect/__main__.py
+++ netdissect/__main__.py
@@ -39,7 +39,7 @@ def main():
     parser.add_argument('--broden', type=str, default=None,
                         help='filename of Broden dataset')
     parser.add_argument('--layers', type=strpair, nargs='+',
-                        help='list of layer names to dissect')
+                        help='space-separated list of layer names to dissect')
     parser.add_argument('--netname', type=str, default=None,
                         help='name for network in generated reports')
     parser.add_argument('--imgsize', type=intpair, default=(227, 227),
@@ -145,11 +145,28 @@ def eval_constructor(term, construct_types=True):
     '''
     from collections import defaultdict
     from importlib import import_module
+
+    class DictNamespace(object):
+        def __init__(self, d):
+            self.__d__ = d
+        def __getattr__(self, key):
+            return self.__d__[key]
+
     class AutoImportDict(defaultdict):
+        def __init__(self, parent=None):
+            super().__init__()
+            self.parent = parent
         def __missing__(self, key):
+            if self.parent is not None:
+                key = self.parent + '.' + key
             if hasattr(__builtins__, key):
                 return getattr(__builtins__, key)
-            return import_module(key)
+            mdl = import_module(key)
+            # Return an AutoImportDict for any namespace packages
+            if hasattr(mdl, '__path__') and not hasattr(mdl, '__file__'):
+                return DictNamespace(AutoImportDict(key))
+            return mdl
+
     obj = eval(term, {}, AutoImportDict())
     if isinstance(obj, type):
         obj = obj()
